4c8b322d4337c86bcfcca4f682288a1b1eca7f28
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-03-29 15:13:09 -0300

    Create EjemploSwitch.cs

---
+++ EjemploSwitch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjemploSwitch : MonoBehaviour
+{
+    public string nombre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (nombre)
+        {
+            case "Jeronimo":
+                Debug.Log("Profe de MeI3D");
+                break;
+            case "Ivo":
+                Debug.Log("Profe de IoT");
+                break;
+            case "Ranzo":
+                Debug.Log("Profe de Front End");
+                break;
+            default:
+                Debug.Log("No es un profe");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
